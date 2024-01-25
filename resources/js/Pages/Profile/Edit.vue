1a3442f2e2c39d31021c052d0ed38c96af2a57e6
jimmy <jimmy@kali.kali>
2024-01-25 11:07:06 +0300

    commit

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -3,6 +3,7 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import DeleteUserForm from './Partials/DeleteUserForm.vue';
 import UpdatePasswordForm from './Partials/UpdatePasswordForm.vue';
 import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm.vue';
+import TwoFactorAuthenticationForm from './Partials/TwoFactorAuthenticationForm.vue';
 import { Head } from '@inertiajs/vue3';
 
 defineProps({
@@ -36,6 +37,9 @@ defineProps({
                 <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                     <UpdatePasswordForm class="max-w-xl" />
                 </div>
+                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                    <TwoFactorAuthenticationForm class="max-w-xl" />
+                </div>
 
                 <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                     <DeleteUserForm class="max-w-xl" />
@@ -44,3 +48,9 @@ defineProps({
         </div>
     </AuthenticatedLayout>
 </template>
+
+<style>
+.flex-col {
+    overflow-y: scroll !important;
+}
+</style>
