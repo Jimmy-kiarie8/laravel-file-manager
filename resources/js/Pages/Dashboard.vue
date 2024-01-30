32151d03b2cb3c882803faa87badcd5e1ceb3627
jimmy <jimmy@kali.kali>
2024-01-30 11:57:40 +0300

    commit

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -3,6 +3,7 @@ import AppLayout from '@/Layouts/AppLayout.vue';
 import Welcome from '@/Components/Welcome.vue';
 </script>
 
+
 <template>
     <AppLayout title="Dashboard">
         <template #header>
