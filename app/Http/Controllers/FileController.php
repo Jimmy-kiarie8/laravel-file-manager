56bc587498124edd8ed8bd4d359d4a3bac08246e
jimmy <jimmy@kali.kali>
2024-02-12 12:56:42 +0300

    commit

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -264,7 +264,8 @@ class FileController extends Controller
         } else {
             [$url, $filename] = $this->getDownloadUrl($ids, $parent->name);
         }
-
+        Log::debug($url);
+        Log::debug(strpos($url, "https"));
         if (strpos($url, "https") === 0) {
             $url = str_replace("https", "http", $url);
         }
